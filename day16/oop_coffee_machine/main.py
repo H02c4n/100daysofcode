fd20824ccf45c72576ffc3789c83011b1dc6d4b0
halil <svtr.hozcan@gmail.com>
2023-05-20 17:24:41 +0200

    report methods

--- day16/oop_coffee_machine/main.py
+++ day16/oop_coffee_machine/main.py
@@ -2,4 +2,8 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+money_machine = MoneyMachine()
+coffe_maker = CoffeeMaker()
 
+money_machine.report()
+coffe_maker.report()
