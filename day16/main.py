b7aadb6284849abad91a492ba377d10f6533c0af
halil <svtr.hozcan@gmail.com>
2023-05-20 17:03:17 +0200

    pretty table add some

--- day16/main.py
+++ day16/main.py
@@ -1,27 +1,30 @@
 
-import another_module
+# import another_module
 
-print(another_module.another_variable)
+# print(another_module.another_variable)
 
-from turtle import Turtle, Screen
-
-
-timmy = Turtle()
-
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
-
-my_screen = Screen()
-
-print(my_screen.canvheight)
-
-my_screen.exitonclick()
+# from turtle import Turtle, Screen
 
 
-print("| Pokemon Name |")
-print("---------------------")
+# timmy = Turtle()
 
-import prettytable
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
 
+# my_screen = Screen()
+
+# print(my_screen.canvheight)
+
+# my_screen.exitonclick()
+
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "water", "Fire"])
+
+print(table)
