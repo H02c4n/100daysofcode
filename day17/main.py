782828750dd76b660ab4bb5880f189d519d2bb2b
halil <svtr.hozcan@gmail.com>
2023-05-21 11:22:06 +0200

    init constructors

--- day17/main.py
+++ day17/main.py
@@ -1,17 +1,24 @@
 class User:
-    def __init__(self):
+    def __init__(self, user_id, username):
         print("new user being created")        
+        self.id = user_id
+        self.username = username
+        self.followers = 0
 
+user_1 = User("001","Halil")
 
-user_1 = User()
-
-user_1.id = "001"
-user_1.username = "Halil"
+# user_1.id = "001"
+# user_1.username = "Halil"
 
 print(user_1.username)
 
 
-user_2 = User()
+user_2 = User("002","Cemal")
 
-user_2.id = "002"
-user_2.username = "Cemal"
+# user_2.id = "002"
+# user_2.username = "Cemal"
+
+user_3 = User("003", "Faik")
+
+print(user_3.username)
+print(user_3.followers)
