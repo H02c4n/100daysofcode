359003c01217afb0e96a44b2dc8d381b50627665
halil <svtr.hozcan@gmail.com>
2023-05-21 11:13:38 +0200

    def__init__

--- day17/main.py
+++ day17/main.py
@@ -1,5 +1,6 @@
 class User:
-    pass
+    def __init__(self):
+        print("new user being created")        
 
 
 user_1 = User()
@@ -8,3 +9,9 @@ user_1.id = "001"
 user_1.username = "Halil"
 
 print(user_1.username)
+
+
+user_2 = User()
+
+user_2.id = "002"
+user_2.username = "Cemal"
