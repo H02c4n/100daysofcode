25a1115903542617430cb671c297dd44b1b8eb20
halil <svtr.hozcan@gmail.com>
2023-05-22 12:31:54 +0200

    check answer method

--- day17/quiz_game/quiz_brain.py
+++ day17/quiz_game/quiz_brain.py
@@ -4,13 +4,24 @@ class QuizBrain:
         self.question_list = q_list
 
     def still_has_questions(self):
-        if self.question_number < len(self.question_list):
+        if self.question_number < len(self.question_list): 
+            ## or just ## return self.question_number < len(self.question_list)
             return True
         else:
             False
-            
+
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False):")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False):")
+        self.check_answer(user_answer, current_question.answer)
+
+
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right")
+        else:
+            print("That is wrong")
+        print(f"The correct answer was: {correct_answer}")
