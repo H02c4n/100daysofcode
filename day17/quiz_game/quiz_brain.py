22e348972c072cb6f68798389e0ed36707252c5b
halil <svtr.hozcan@gmail.com>
2023-05-22 12:34:29 +0200

    add user score

--- day17/quiz_game/quiz_brain.py
+++ day17/quiz_game/quiz_brain.py
@@ -1,6 +1,7 @@
 class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
+        self.score = 0
         self.question_list = q_list
 
     def still_has_questions(self):
@@ -21,7 +22,9 @@ class QuizBrain:
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
+            self.score += 1
             print("You got it right")
         else:
             print("That is wrong")
         print(f"The correct answer was: {correct_answer}")
+        print(f"Tour current score is : {self.score}/{self.question_number}")
