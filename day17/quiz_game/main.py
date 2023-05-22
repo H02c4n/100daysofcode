0c60953e0335f08816ff2b98b00b658d10b09e68
halil <svtr.hozcan@gmail.com>
2023-05-22 12:06:40 +0200

    create question list from data

--- day17/quiz_game/main.py
+++ day17/quiz_game/main.py
@@ -1,10 +1,12 @@
-class Question:
+from question_model import Question
+from data import question_data
 
-    def __init__(self, q_text, q_answer):
-        self.text = q_text
-        self.answer = q_answer
+question_bank = []
 
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(q_text=question_text, q_answer=question_answer)
+    question_bank.append(new_question)
 
-new_q = Question("sdfs", "sdfsf")
-
-print(new_q.text)
+print(question_bank)
