cff51b436244dbd222c33c0ce594eeb1fe710dcc
halil <svtr.hozcan@gmail.com>
2023-05-23 14:24:10 +0200

    check correct answer

--- day17/quiz_v2/quiz.py
+++ day17/quiz_v2/quiz.py
@@ -14,4 +14,27 @@ class Quiz:
         answers = current_question.wrong_answers
         answers.append(current_question.correct_answer)
         random.shuffle(answers)
-        input(f"Q.{self.question_number + 1} : {current_question.question_text}\nA:{answers[0]}\nB:{answers[1]}\nC:{answers[2]}\nD:{answers[3]}\n(Your answer A-B-C-D)")
+        user_answer = input(f"Q.{self.question_number + 1} : {current_question.question_text}\nA:{answers[0]}\nB:{answers[1]}\nC:{answers[2]}\nD:{answers[3]}\nYour answer (A-B-C-D):").lower()
+        if user_answer == "a":
+            user_answer = answers[0]
+        elif user_answer == "b":
+            user_answer = answers[1]
+        elif user_answer == "c":
+            user_answer = answers[2]
+        elif user_answer == "d":
+            user_answer = answers[3]
+        else:
+            user_answer
+
+        self.check_answer(user_answer, current_question.correct_answer)
+
+
+    
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("You got it right")
+        else:
+            print("That is wrong")
+        print(self.score)
