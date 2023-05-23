49b9a013df08c6144ff7dc4a6b656fa18c6fea34
halil <svtr.hozcan@gmail.com>
2023-05-23 12:32:09 +0200

    question model is created

---
+++ day17/quiz_v2/question.py
@@ -0,0 +1,6 @@
+class Question:
+
+    def __init__(self, c_answer, w_answer, q_text):
+        self.question_text = q_text
+        self.correct_answer = c_answer
+        self.wrong_answer = w_answer
