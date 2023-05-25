3a87781aad33948bd23ab3c7af1c1218a06c9f79
halil <svtr.hozcan@gmail.com>
2023-05-25 12:15:04 +0200

    Draw a random walk

--- day18/main.py
+++ day18/main.py
@@ -18,23 +18,31 @@ timmy = Turtle()
 #     timmy.forward(10)
 #     timmy.pendown()
 
-#! Draw a triangle square pentagon hexagon.....
+# #! Draw a triangle square pentagon hexagon.....
 
 colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
-def draw_shape(num_sides):
-    angle = 360/num_sides
-    for _ in range(num_sides):
-        timmy.forward(100)
-        timmy.right(angle)
+# def draw_shape(num_sides):
+#     angle = 360/num_sides
+#     for _ in range(num_sides):
+#         timmy.forward(100)
+#         timmy.right(angle)
 
-for shape_side in range(3,11):
+# for shape_side in range(3,11):
+#     timmy.color(random.choice(colors))
+#     draw_shape(shape_side)
+
+
+#! Draw a random walk
+
+directions =[0,90,180,270]
+timmy.pensize(10)
+timmy.speed(0)
+
+for _ in range(200):
     timmy.color(random.choice(colors))
-    draw_shape(shape_side)
-
-
-
-
+    timmy.forward(30)
+    timmy.setheading(random.choice(directions))
 
 
 
