ac7d02fd80dd6e075c6fa75445210a4d39cc7adb
halil <svtr.hozcan@gmail.com>
2023-05-25 11:41:29 +0200

    draw a dashed line

--- day18/main.py
+++ day18/main.py
@@ -2,14 +2,21 @@ from turtle import Turtle, Screen
 
 timmy = Turtle()
 
-timmy.shape("turtle")
+# timmy.shape("turtle")
 # timmy.color("red")
 # timmy.forward(100)
 
+#! draw a square
 for _ in range(4):
     timmy.forward(100)
     timmy.left(90)
 
+#! Draw a dashed line
+for _ in range(15):
+    timmy.forward(10)
+    timmy.penup()
+    timmy.forward(10)
+    timmy.pendown()
 
 
 
