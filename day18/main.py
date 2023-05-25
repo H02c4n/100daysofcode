da927383536b4767d79b71139086c3225303cd28
halil <svtr.hozcan@gmail.com>
2023-05-25 12:28:41 +0200

    generate rgb random colors

--- day18/main.py
+++ day18/main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
 import random
 timmy = Turtle()
 
@@ -35,12 +35,21 @@ colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaG
 
 #! Draw a random walk
 
+colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    return (r,g,b)
+
 directions =[0,90,180,270]
 timmy.pensize(10)
-timmy.speed(0)
+timmy.speed("fastest")
 
 for _ in range(200):
-    timmy.color(random.choice(colors))
+    timmy.color(random_color())
+    #timmy.color(random.choice(colors))
     timmy.forward(30)
     timmy.setheading(random.choice(directions))
 
