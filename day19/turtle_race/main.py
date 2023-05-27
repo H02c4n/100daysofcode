d487cbe5301d76a58ac0234c0ac3740d146d9903
halil <svtr.hozcan@gmail.com>
2023-05-27 11:58:22 +0200

    add 6 turtle on the screen

---
+++ day19/turtle_race/main.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+import random
+
+
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_pos = [-70,-40,-10,20,50,80]
+
+for turtle_index in range(0,6):
+    tim = Turtle(shape="turtle")
+    tim.penup()
+    tim.goto(x=-230 , y=y_pos[turtle_index])
+
+
+
+
+
+screen.exitonclick()
