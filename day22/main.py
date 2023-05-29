7ec4b62e9b6009f4d4333df91c31705a0f39510d
halil <svtr.hozcan@gmail.com>
2023-05-29 11:12:29 +0200

    create a padlle and move the paddle up and down

--- day22/main.py
+++ day22/main.py
@@ -1,4 +1,4 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 
 
@@ -15,7 +15,29 @@ screen.title("Pong")
 
 
 
+paddle = Turtle()
+paddle.shape("square")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350,0)
 
+
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
 
 
 
