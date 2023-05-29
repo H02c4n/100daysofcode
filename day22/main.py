66d80d23cb6adca3ec515795287a17acdbef9ad1
halil <svtr.hozcan@gmail.com>
2023-05-29 11:53:41 +0200

    create ball class and take an inst from this class

--- day22/main.py
+++ day22/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 
 
 
@@ -16,6 +17,8 @@ screen.tracer(0)
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
+ball =Ball()
+
 
 # paddle = Turtle()
 # paddle.shape("square")
