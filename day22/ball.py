c54b2914c8bb0cf61b0a1142dd4a443f757ed8bd
halil <svtr.hozcan@gmail.com>
2023-05-29 12:59:24 +0200

    inc ball speed

--- day22/ball.py
+++ day22/ball.py
@@ -11,6 +11,7 @@ class Ball(Turtle):
         self.penup()
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
     
 
     def move(self):
@@ -26,6 +27,7 @@ class Ball(Turtle):
 
     def hit_paddle(self):
         self.x_move *= -1
+        self.move_speed *= 0.9
 
 
     def reset_position(self):
