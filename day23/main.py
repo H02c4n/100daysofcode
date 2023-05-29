86cdbbee10c0d7a7c2f98f3a2797c15b9d9bdec0
halil <svtr.hozcan@gmail.com>
2023-05-29 21:00:15 +0200

    listen the screen and make move the turtle

--- day23/main.py
+++ day23/main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 import time
 
+from player import Player
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -8,9 +10,23 @@ screen.tracer(0)
 
 
 
+timmy = Player()
+
+
+screen.listen()
+screen.onkey(timmy.go_up, "Up")
+
 
 game_is_on = True
 
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+
+
+
+
+
+
+screen.exitonclick()
