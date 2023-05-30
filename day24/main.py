56b2dcd1ef881f65bf169b192a4b154c539bb669
halil <svtr.hozcan@gmail.com>
2023-05-30 11:10:48 +0200

    create folder and file for mail_merge project

--- day24/main.py
+++ day24/main.py
@@ -2,15 +2,13 @@ from turtle import Screen
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
-import time 
-
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("SNAKE GAME")
 screen.tracer(0)
-
 
 snake = Snake()
 food = Food()
@@ -22,21 +20,19 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-    
-    #Detect collision with food
+
+    # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
 
-
-    #Detect collision with wall.
+    # Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         scoreboard.reset()
         snake.reset()
@@ -49,7 +45,7 @@ while game_is_on:
     #         game_is_on = False
     #         scoreboard.game_over()
 
-    #Detect collision with tail. with slicing
+    # Detect collision with tail. with slicing
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
