d97c68bf5a70a925cf3bb23c2c87f96f6aa001fb
halil <svtr.hozcan@gmail.com>
2023-05-30 12:46:05 +0200

    read data inside csv file

--- day25/main.py
+++ day25/main.py
@@ -3,5 +3,8 @@ import csv
 
 with open("weather_data.csv") as data_file:
     data = csv.reader(data_file)
+    temperatures = []
     for row in data:
-        print(row)
+        temperatures.append(row[1])
+    temperatures = temperatures[1:]
+    print(temperatures)
