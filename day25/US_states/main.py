3fde36d84e6e015911f03ece31016fe23cfcd33f
halil <svtr.hozcan@gmail.com>
2023-05-31 19:19:50 +0200

    get the user answer with textinput method

--- day25/US_states/main.py
+++ day25/US_states/main.py
@@ -7,11 +7,15 @@ screen.addshape(image)
 turtle.shape(image)
 
 
-def get_mouse_click_coor(x,y):
-    print(x,y)
+# def get_mouse_click_coor(x,y):
+#     print(x,y)
 
-turtle.onscreenclick(get_mouse_click_coor)
+# turtle.onscreenclick(get_mouse_click_coor)
 
-turtle.mainloop()
+# turtle.mainloop()
 
 #screen.exitonclick()
+
+
+answer_state = screen.textinput(title="Guess the state", prompt="What is another state's name")
+
