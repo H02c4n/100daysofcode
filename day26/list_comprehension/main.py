7e2faf5253a91f0890c0d36a3626c276546a73f8
halil <svtr.hozcan@gmail.com>
2023-06-01 11:12:40 +0200

    even number with list comp

--- day26/list_comprehension/main.py
+++ day26/list_comprehension/main.py
@@ -31,3 +31,7 @@ numbers = [1,1,2,3,5,8,13,21,34,55]
 squared_numbers = [num**2 for num in numbers]
 
 print(squared_numbers)
+
+even_numbers = [n for n in numbers if n % 2 == 0]
+
+print(even_numbers)
