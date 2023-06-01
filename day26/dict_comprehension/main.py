6f2a7dfea226ce9d8985f3f59eea8e4e68ac5122
halil <svtr.hozcan@gmail.com>
2023-06-01 12:40:15 +0200

    return students which passed 50

--- day26/dict_comprehension/main.py
+++ day26/dict_comprehension/main.py
@@ -11,4 +11,6 @@ students_score = {name:random.randint(1,100) for name in names}
 
 print(students_score)
 
-passed_students = {name:}
+passed_students = {name:score for (name, score) in students_score.items() if score > 50 }
+
+print(passed_students)
