12b62d305e58a421ad3e3046eec3827e8a2e0b5e
halil <svtr.hozcan@gmail.com>
2023-06-02 10:32:52 +0200

    *args =>  many pos arg

--- day27/main.py
+++ day27/main.py
@@ -4,9 +4,13 @@ window = tkinter.Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=300)
 
-
 #Label
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label.pack(side="left")  # expand="True"
+
+
+
+
+
 
 window.mainloop()
---
+++ day27/playground.py
@@ -0,0 +1,7 @@
+def add(*args):
+    for n in args:
+        print(n)
+    print(type(args))
+
+
+add(3,4,5)
