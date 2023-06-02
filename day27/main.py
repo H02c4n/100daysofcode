55e007cbe0aaabd7e95b6b99928ca80f776b447c
halil <svtr.hozcan@gmail.com>
2023-06-02 11:02:14 +0200

    use button in tkinter

--- day27/main.py
+++ day27/main.py
@@ -1,16 +1,25 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=300)
 
-#Label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")  # expand="True"
+# Label
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()  # expand="True" or side="left"
+
+my_label["text"] = "new Text"
+my_label.config(text="New Text")
 
 
+# Button
+
+def button_clicked():
+    print("I got clicked")
+    my_label.config(text="Last version")
 
 
-
+button = Button(text="Calculate", command=button_clicked)
+button.pack()
 
 window.mainloop()
