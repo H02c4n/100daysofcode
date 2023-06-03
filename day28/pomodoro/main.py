a0c27157357d0bac169d25aa707cc0aa9e363ef4
halil <svtr.hozcan@gmail.com>
2023-06-03 12:26:59 +0200

    timer reset function

--- day28/pomodoro/main.py
+++ day28/pomodoro/main.py
@@ -10,8 +10,17 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
@@ -41,7 +50,8 @@ def count_down(count):
         count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
         marks = ""
@@ -68,7 +78,7 @@ canvas.grid(column=1, row=1)
 
 start_button = Button(text="Start", command=start_timer, highlightthickness=0, bg=GREEN, fg="white", font=(FONT_NAME, 11, "bold"))
 start_button.grid(column=0, row=2)
-reset_button = Button(text="Reset", highlightthickness=0, bg=RED, fg="white", font=(FONT_NAME, 11, "bold"))
+reset_button = Button(text="Reset", command=reset_timer, highlightthickness=0, bg=RED, fg="white", font=(FONT_NAME, 11, "bold"))
 reset_button.grid(column=2, row=2)
 
 check_marks = Label(fg=GREEN, bg=YELLOW)
