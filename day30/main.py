b2188d3f0f9e0acee090b5f98a7c438056ce62b1
halil <svtr.hozcan@gmail.com>
2023-06-04 13:01:44 +0200

    raise an error

--- day30/main.py
+++ day30/main.py
@@ -1,15 +1,26 @@
-try:
-    file = open("a_file.txt")
-    a_dict = {"key": "value"}
-    print(a_dict["key"])
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("something")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist.")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File was closed")
+# try:
+#     file = open("a_file.txt")
+#     a_dict = {"key": "value"}
+#     print(a_dict["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("File was closed")
+#     raise TypeError("This is an error that I made up.")
+
+height = float(input("Height"))
+weight = float(input("Weight"))
+
+
+if height > 3:
+    raise ValueError("Human height should not be over 3 meter")
+
+bmi = weight / height ** 2
+print(bmi)
