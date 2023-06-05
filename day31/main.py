714a9c270509db88012cf9088aedf71f0ef7f01b
halil <svtr.hozcan@gmail.com>
2023-06-05 23:41:17 +0200

    flip the card from english to turkish

--- day31/main.py
+++ day31/main.py
@@ -6,14 +6,18 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("data/entr.csv")
 to_learn = data.to_dict(orient="records")
-print(to_learn)
+curren_card = {}
 
 def next_card():
+    global curren_card
     current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="English")
     canvas.itemconfig(card_word, text=current_card["English"])
 
 
+def flip_card():
+    canvas.itemconfig(card_title, text="Turkish")
+    canvas.itemconfig(card_word, text=curren_card["Turkish"])
 
 
 
@@ -22,12 +26,14 @@ window = Tk()
 window.title("FlashCard")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+window.after(3000, func=flip_card)
+
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
@@ -38,6 +44,8 @@ unknown_button.grid(row=1, column=0)
 check_image = PhotoImage(file="images/right.png")
 right_button = Button(image=check_image, highlightthickness=0, command=next_card)
 right_button.grid(row=1, column=1)
+
+next_card()
 
 
 window.mainloop()
