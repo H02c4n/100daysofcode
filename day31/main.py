e996e3da1b51cd48ecfd3fc51d14f4238d835806
halil <svtr.hozcan@gmail.com>
2023-06-05 23:05:13 +0200

    window is created

--- day31/main.py
+++ day31/main.py
@@ -1,3 +1,17 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
 
+window = Tk()
+window.title("FlashCard")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+
+
+
+
+
+
+
+window.mainloop()
