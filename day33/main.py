900860fea95dd7a3b962b98d86a04a1ff5af7bde
halil <svtr.hozcan@gmail.com>
2023-06-07 13:15:41 +0200

    sunrise sunset api

--- day33/main.py
+++ day33/main.py
@@ -1,14 +1,31 @@
 import requests
 
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
 
-data = response.json()
+# data = response.json()
 
-longitude = data['iss_position']['longitude']
-latitude = data['iss_position']['latitude']
+# longitude = data['iss_position']['longitude']
+# latitude = data['iss_position']['latitude']
 
-iss_pos = (longitude, latitude)
+# iss_pos = (longitude, latitude)
 
-print(iss_pos)
+# print(iss_pos)
+
+parameters = {
+    "lat":51.507351,
+    "lng":-0.127758,
+    "formatted":0
+}
+
+res = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+res.raise_for_status()
+data = res.json()
+
+sunrise = data['results']['sunrise']
+sunrise = sunrise.split("T")[1]
+sunset = data['results']['sunset']
+sunset = sunset.split("T")[1]
+
+print(f"({sunrise}, {sunset})")
