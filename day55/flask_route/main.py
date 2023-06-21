4fe3ba5d0bfb73dd5d5a5085cb47fae1f782e0bc
halil <svtr.hozcan@gmail.com>
2023-06-21 13:17:17 +0200

    advenced python decorator function

--- day55/flask_route/main.py
+++ day55/flask_route/main.py
@@ -3,12 +3,33 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(fn):
+    def wrapper():
+        return "<b>" + fn() + "</b>"
+
+    return wrapper
+
+
+def make_emphasis(fn):
+    def wrapper():
+        return "<em>" + fn() + "</em>"
+    return wrapper
+
+def make_underline(fn):
+    def wrapper():
+        return "<u>" + fn() + "</u>"
+    return wrapper
+
+
 @app.route("/")
 def hello():
     return "<p>Hello World</p>"
 
 
 @app.route("/hi")
+@make_bold
+@make_emphasis
+@make_underline
 def hi():
     return "<p>Hi</p>"
 
@@ -19,4 +40,5 @@ def greet(ad, yas):
 
 
 if __name__ == "__main__":
+    # Run the app in debug mode to auto-reload
     app.run(debug=True)
